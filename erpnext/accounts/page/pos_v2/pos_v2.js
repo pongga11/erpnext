bf4a4c262ea1c8e7048030465064c0e406aa7c93
Vick (AI) <vick@baanyayim.com>
2026-03-12 00:10:41 +0700

    Phase 1.5-1.7: V2 JS with layout override + Templates + CSS
    - Update pos_v2.js with make_control override for V2 layout
    - Add localStorage key overrides (_v2 suffix)
    - Add pos_v2_item_card.html template
    - Add pos_v2_cart_item.html template
    - Add animations.css
    - Add responsive.css

--- erpnext/accounts/page/pos_v2/pos_v2.js
+++ erpnext/accounts/page/pos_v2/pos_v2.js
@@ -24,3 +24,69 @@ frappe.pages['pos-v2'].refresh = function (wrapper) {
 		return wrapper.pos.beforeunload()
 	}
 }
+
+// Override make_control to use V2 layout template
+erpnext.pos.PointOfSale.prototype.make_control_original = erpnext.pos.PointOfSale.prototype.make_control;
+erpnext.pos.PointOfSale.prototype.make_control = function() {
+	this.frm = {}
+	this.frm.doc = this.doc
+	this.set_transaction_defaults("Customer");
+	this.frm.doc["allow_user_to_edit_rate"] = this.pos_profile_data["allow_user_to_edit_rate"] ? true : false;
+	
+	// Use V2 layout template
+	this.wrapper.html(frappe.render_template("pos_v2_layout", this.frm.doc));
+	
+	this.make_search();
+	this.make_customer();
+	this.make_list_customers();
+	this.bind_numeric_keypad();
+}
+
+// Override localStorage keys to avoid conflict with original POS
+erpnext.pos.PointOfSale.prototype.get_doc_from_localstorage = function () {
+	try {
+		return JSON.parse(localStorage.getItem('sales_invoice_doc_v2')) || [];
+	} catch (e) {
+		return []
+	}
+};
+
+erpnext.pos.PointOfSale.prototype.update_localstorage = function () {
+	try {
+		localStorage.setItem('sales_invoice_doc_v2', JSON.stringify(this.si_docs));
+	} catch (e) {
+		frappe.throw(__("LocalStorage is full, did not save"))
+	}
+};
+
+erpnext.pos.PointOfSale.prototype.get_email_queue = function () {
+	try {
+		return JSON.parse(localStorage.getItem('email_queue_v2')) || {};
+	} catch (e) {
+		return {}
+	}
+};
+
+erpnext.pos.PointOfSale.prototype.update_email_queue = function () {
+	try {
+		localStorage.setItem('email_queue_v2', JSON.stringify(this.email_queue));
+	} catch (e) {
+		frappe.throw(__("LocalStorage is full, did not save"))
+	}
+};
+
+erpnext.pos.PointOfSale.prototype.get_customers_details = function () {
+	try {
+		return JSON.parse(localStorage.getItem('customer_details_v2')) || {};
+	} catch (e) {
+		return {}
+	}
+};
+
+erpnext.pos.PointOfSale.prototype.update_customer_in_localstorage = function() {
+	try {
+		localStorage.setItem('customer_details_v2', JSON.stringify(this.customer_details));
+	} catch (e) {
+		frappe.throw(__("LocalStorage is full, did not save"))
+	}
+};
