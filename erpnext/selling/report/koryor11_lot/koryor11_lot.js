ed54fd6c9aaa8350278b34e7caa4ae7b8b70a63f
Ratipong Pele <>
2026-03-12 13:36:01 +0700

    report filter

--- erpnext/selling/report/koryor11_lot/koryor11_lot.js
+++ erpnext/selling/report/koryor11_lot/koryor11_lot.js
@@ -1,24 +1,43 @@
 frappe.query_reports["KorYor11-LOT"] = {
-    "filters": [
+    filters: [
         {
-            "fieldname": "from_date",
-            "label": "From",
-            "fieldtype": "Date",
-            "reqd": 1,
+            fieldname: "item_code",
+            label: "Item Code",
+            fieldtype: "Link",
+            options: "Item",
+            reqd: 1,
+            get_query: function() {
+                return {
+                    filters: [
+                        ["Item", "is_khoryor", "=", 1],
+                        ["Item", "has_batch_no", "=", 1],
+                        ["Item", "disabled", "=", 0]
+                    ]
+                };
+            },
+            on_change: function() {
+                // เมื่อเปลี่ยน item ให้ clear batch
+                frappe.query_report.set_filter_value("batch_no", "");
+            }
         },
         {
-            "fieldname": "to_date",
-            "label": "To",
-            "fieldtype": "Date",
-            "reqd": 1,
-        },
-        {
-            "fieldname": "item_code",
-            "label": "Item",
-            "fieldtype": "Link",
-            "options": "Item",
-            "reqd": 1,
-        },
+            fieldname: "batch_no",
+            label: "Batch No",
+            fieldtype: "Link",
+            options: "Batch",
+            get_query: function() {
+                let item_code = frappe.query_report.get_filter_value("item_code");
+                if (!item_code) {
+                    frappe.msgprint("กรุณาเลือก Item ก่อน");
+                    return { filters: [["Batch", "name", "=", ""]] };
+                }
+                return {
+                    filters: [
+                        ["Batch", "item", "=", item_code],
+                        ["Batch", "disabled", "=", 0]
+                    ]
+                };
+            }
+        }
     ]
-}
-
+};
