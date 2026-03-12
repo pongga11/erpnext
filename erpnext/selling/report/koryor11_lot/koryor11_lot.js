6ab005f334777cedb6313a657c59a1435eeea3df
Ratipong Pele <>
2026-03-12 13:45:47 +0700

    fix report filter

--- erpnext/selling/report/koryor11_lot/koryor11_lot.js
+++ erpnext/selling/report/koryor11_lot/koryor11_lot.js
@@ -16,9 +16,14 @@ frappe.query_reports["KorYor11-LOT"] = {
                 };
             },
             on_change: function() {
-                let batch_filter = frappe.query_report.get_filter("batch_no");
-                if (batch_filter) {
-                    batch_filter.set_value("");
+                let filters = frappe.query_report.filters;
+                for (let i = 0; i < filters.length; i++) {
+                    if (filters[i].df && filters[i].df.fieldname === "batch_no") {
+                        filters[i].value = "";
+                        filters[i].last_value = "";
+                        filters[i].$input.val("");
+                        break;
+                    }
                 }
             }
         },
@@ -28,7 +33,10 @@ frappe.query_reports["KorYor11-LOT"] = {
             fieldtype: "Link",
             options: "Batch",
             get_query: function() {
-                let item_code = frappe.query_report.get_filter_value("item_code");
+                let item_code = frappe.query_report.filters.find(
+                    f => f.df.fieldname === "item_code"
+                )?.value;
+
                 if (!item_code) {
                     frappe.msgprint("กรุณาเลือก Item ก่อน");
                     return { filters: [["Batch", "name", "=", ""]] };
